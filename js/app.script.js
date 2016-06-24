ad133671b8d45357a3028a2e8e20f9f26e91acfa
Oswaldo Peña <oswaldopr@gmail.com>
2016-06-23 23:32:29 -0430

    Scripts CSS y JS de la aplicación
    Hoja de estilos y funciones javascript de uso común.

---
+++ js/app.script.js
@@ -0,0 +1,12 @@
+function getController() {
+    return $F("hfController");
+}
+
+function isKeyRETURN(eventHandler) {
+    return (eventHandler.keyCode == Event.KEY_RETURN);
+}
+
+function applyKeyRETURN(eventHandler, button) {
+    if(isKeyRETURN(eventHandler))
+        button.click();
+}
